357c1afa18d4efeda08806ff7486d063ca9160e6
Florence Lee <florenceclee@Florence-Lees-MacBook.local>
2014-12-09 00:15:32 -0500

    more interesting Shiny graph

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,24 +1,90 @@
 library(shiny)
+library(scales)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
+#draw outline of national data; static
+#ebolaUse <- subset(ebolaData, ebolaData$Localite %in% "National" & ebolaData$Category %in% "Cases")
+#data <- ggplot(ebolaUse, aes(Date, Value))
+#ebGph <- geom_line(aes(colour=factor(Country)), size=1, alpha=0.3)
+#dateScale <- scale_x_date(breaks=date_breaks("months"), labels=date_format("%m/%d/%y"))
+#labels <- labs(title="Ebola Cases in Africa Over Time", x="Date", y="Number of Cases", colour="Country")
+#cols <- scale_colour_manual(values = c("#11887F", "#FC1325", "#670155"))
+#format <- theme(panel.background = element_rect(fill="white"),
+#                panel.grid.major = element_line(colour="#CFCFCF", linetype=2),
+#                axis.line = element_line(size=0.25, colour="black"),
+##                axis.title = element_text(family="Open Sans", size=14),
+#                plot.title = element_text(family="Open Sans", size=18),
+#                axis.text = element_text(family="Open Sans", size=12, colour="black"),
+#                axis.ticks = element_line(size=0.25, colour="black"))  
+
+shinyServer(function(input, output) {  
   output$ebPlot <- renderPlot({
-    data <- switch(input$cat, 
-                   "Cases"="Cases",
-                   "Deaths"="Deaths")
-    ebolaUse <- subset(ebolaData, ebolaData$Localite %in% "National" & ebolaData$Category %in% data)
+    var <- switch(input$loc, 
+                  "National"="National",
+                  "Conakry"="Conakry",
+                  "Gueckedou"="Gueckedou", 
+                  "Kissidougou"="Kissidougou", 
+                  "Macenta"="Macenta", 
+                  "Dabola"="Dabola", 
+                  "Dinguiraye"="Dinguiraye", 
+                  "Telimele"="Telimele", 
+                  "Boffa"="Boffa", 
+                  "Kouroussa"="Kouroussa",   
+                  "Dubreka"="Dubreka", 
+                  "Nzerekore"="Nzerekore", 
+                  "Pita"="Pita", 
+                  "Siguiri"="Siguiri", 
+                  "Yomou"="Yomou", 
+                  "Forecariah"="Forecariah", 
+                  "Kerouane"="Kerouane", 
+                  "Coyah"="Coyah", 
+                  "Dalaba"="Dalaba", 
+                  "Kindia"="Kindia", 
+                  "Beyla"="Beyla", 
+                  "Lola"="Lola", 
+                  "Faranah"="Faranah", 
+                  "Boke"="Boke", 
+                  "Mamou"="Mamou", 
+                  "Kankan"="Kankan", 
+                  "Kailahun"="Kailahun", 
+                  "Bo"="Bo", 
+                  "Kenema"="Kenema", 
+                  "Koinadugu"="Koinadugu", 
+                  "Moyamba"="Moyamba", 
+                  "Bombali"="Bombali",
+                  "Freetown"="Freetown", 
+                  "Kambia"="Kambia", 
+                  "Kono"="Kono", 
+                  "Port Loko"="Port Loko", 
+                  "Western Area"="Western Area", 
+                  "Bonthe"="Bonthe", 
+                  "Pujehun"="Pujehun", 
+                  "Tonkolili"="Tonkolili", 
+                  "Western Area Rural"="Western Area Rural", 
+                  "Western Area Urban"="Western Area Urban", 
+                  "Bong"="Bong", 
+                  "Grand Cape Mount"="Grand Cape Mount", 
+                  "Lofa"="Lofa", 
+                  "Margibi"="Margibi", 
+                  "Montserrado"="Montserrado", 
+                  "Nimba"="Nimba", 
+                  "Grand Gedeh"="Grand Gedeh", 
+                  "Bomi"="Bomi", 
+                  "Grand Bassa"="Grand Bassa", 
+                  "River Gee"="River Gee", 
+                  "RiverCess"="RiverCess", 
+                  "Sinoe"="Sinoe", 
+                  "Gbarpolu"="Gbarpolu",          
+                  "Maryland"="Maryland", 
+                  "Grand Kru"="Grand Kru"       
+                  )
+    ebolaUse <- subset(ebolaData, ebolaData$Localite %in% "National" & ebolaData$Category %in% "Cases")
+    ebolaUse2 <- subset(ebolaData, ebolaData$Localite %in% var & ebolaData$Category %in% "Cases")
     data <- ggplot(ebolaUse, aes(Date, Value))
-    ebGph <- geom_line(aes(colour=factor(Country)), size=1)
+    ebGph <- geom_line(aes(colour=factor(Country)), size=1, alpha=0.25)
+    locGph <- geom_line(data = ebolaUse2, aes(Date, Value, colour=factor(Country)), size=1)  
     dateScale <- scale_x_date(breaks=date_breaks("months"), labels=date_format("%m/%d/%y"))
-    labels <- labs(title="Ebola Outbreak in Africa Over Time", x="Date", y="Value", colour="Country")
+    labels <- labs(title="Ebola Cases in Africa Over Time", x="Date", y="Number of Cases", colour="Country")
     cols <- scale_colour_manual(values = c("#11887F", "#FC1325", "#670155"))
     format <- theme(panel.background = element_rect(fill="white"),
                     panel.grid.major = element_line(colour="#CFCFCF", linetype=2),
@@ -26,7 +92,7 @@ shinyServer(function(input, output) {
                     axis.title = element_text(family="Open Sans", size=14),
                     plot.title = element_text(family="Open Sans", size=18),
                     axis.text = element_text(family="Open Sans", size=12, colour="black"),
-                    axis.ticks = element_line(size=0.25, colour="black"))     
-    data + ebGph + cols + dateScale + labels  + format
+                    axis.ticks = element_line(size=0.25, colour="black"))  
+    data + ebGph + locGph + cols + dateScale + labels  + format 
   })
 })
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -7,10 +7,22 @@ shinyUI(fluidPage(
   titlePanel("Ebola Outbreak in Africa"),
   
   sidebarLayout(
-    selectInput("cat", 
-                label = "Category",
-                choices = c("Cases", "Deaths"),
-                selected = "Cases"),
+    selectInput("loc", 
+                label = "Localite",
+                choices = c("National", "Conakry", "Gueckedou", "Kissidougou", "Macenta", 
+                            "Dabola", "Dinguiraye", "Telimele", "Boffa", "Kouroussa", 
+                            "Dubreka", "Nzerekore", "Pita", "Siguiri", "Yomou", 
+                            "Forecariah", "Kerouane", "Coyah", "Dalaba", "Kindia", 
+                            "Beyla", "Lola", "Faranah", "Boke", "Mamou", 
+                            "Kankan", "Kailahun", "Bo", "Kenema", "Koinadugu", 
+                            "Moyamba", "Bombali", "Freetown", "Kambia", "Kono", 
+                            "Port Loko", "Western Area", "Bonthe", "Pujehun", "Tonkolili", 
+                            "Western Area Rural", "Western Area Urban", "Bong", "Grand Cape Mount", "Lofa", 
+                            "Margibi", "Montserrado", "Nimba", "Grand Gedeh", "Bomi", 
+                            "Grand Bassa", "River Gee", "RiverCess", "Sinoe", "Gbarpolu",          
+                            "Maryland", "Grand Kru"       
+                            ),
+                selected = "National"),
     
     # Show a plot of the generated distribution
       mainPanel(
